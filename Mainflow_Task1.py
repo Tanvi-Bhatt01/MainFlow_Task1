f08cc0dab0ef53e83fd6bdff53cff54de853295c
Tanvi-Bhatt01 <bhatttanvi06@gmail.com>
2024-05-21 20:12:14 +0530

    Add files via upload

---
+++ Mainflow_Task1.py
@@ -0,0 +1,14 @@
+#creating a list
+my_list = [1,2,3,4,5]
+print("Original list was: ", my_list)
+
+#adding element to list
+my_list.append(6)
+
+#removing element from list
+my_list.remove(4)
+
+#modifying an element in the list
+my_list[0] = 20
+
+print("Updated list is: ", my_list)
